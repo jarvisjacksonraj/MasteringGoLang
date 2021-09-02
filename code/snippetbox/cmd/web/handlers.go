cbc9a83032221f27c78d174ebb3ea0c6f806d7d7
jarvisjacksonraj <53148568+jarvisjacksonraj@users.noreply.github.com>
2021-09-02 23:58:12 +0530

    HTML Templating and Inheritance 1.2

--- code/snippetbox/cmd/web/handlers.go
+++ code/snippetbox/cmd/web/handlers.go
@@ -17,8 +17,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
 	}
-
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
